e7f21da981d6ffe82f6fe99eed0b1e21c724cb92
M M Danish <danishsadpara@gmail.com>
2024-02-10 19:02:43 +0500

    LEARN: routes Group

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { Typography } from "@mui/material";
+import React from "react";
+
+const Register = () => {
+  return (
+    <div>
+      Register page and also (auth) means that in url the auth will be omitted
+      <Typography variant="caption">
+        (actual url is as: 3000/auth/register)
+      </Typography>
+    </div>
+  );
+};
+
+export default Register;
